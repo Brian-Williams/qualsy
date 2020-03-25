cd0a3315f934142bd42f30542d4994a25b5abde5
Brian-Williams <briancmwilliams@gmail.com>
2020-03-25 15:20:25 -0400

    Rename funcs

--- cmd/internal/qualys/qualys.go
+++ cmd/internal/qualys/qualys.go
@@ -11,6 +11,11 @@ import (
 	"net/http"
 )
 
+const (
+	SUCCESS = "SUCCESS"
+)
+
+// Code has the "responseCode" which should be present in all Qualys api returns
 type Code struct {
 	ResponseCode string `xml:"responseCode"`
 }
@@ -29,21 +34,23 @@ type Hostasset_Delete struct {
 	} `xml:"data"`
 }
 
-type qualys struct {
+type Qualys struct {
 	user     string
 	password string
 	api      string
 }
 
-func New(user, password, api string) qualys {
-	return qualys{user, password, api}
+func New(user, password, api string) Qualys {
+	return Qualys{user, password, api}
 }
 
-func (q qualys) baiscAuth() string {
+func (q Qualys) baiscAuth() string {
 	return base64.StdEncoding.EncodeToString([]byte(q.user + ":" + q.password))
 }
 
-func (q qualys) do(method, url string, body io.Reader) (*http.Response, error) {
+// do is an opinionated call
+// It handles headers, client, and considers non-200 status codes an error
+func (q Qualys) do(method, url string, body io.Reader) (*http.Response, error) {
 	end := q.api + url
 
 	req, err := http.NewRequest(method, end, body)
@@ -51,7 +58,7 @@ func (q qualys) do(method, url string, body io.Reader) (*http.Response, error) {
 		return nil, fmt.Errorf("failed to make request for url %v: %w", end, err)
 	}
 
-	req.Header.Add("X-requested-with", "qualys-go")
+	req.Header.Add("X-requested-with", "Qualys-go")
 	req.Header.Add("Content-type", "text/xml")
 	req.Header.Add("Authorization", "Basic "+q.baiscAuth())
 	//req.Header.Add("'cache-control", "no-cache")
@@ -74,7 +81,9 @@ func (q qualys) do(method, url string, body io.Reader) (*http.Response, error) {
 	return resp, nil
 }
 
-func (q qualys) post(url string, body io.Reader) (*http.Response, error) {
+// post is an opinionated post to Qualys.
+// It will try to make an invalid call valid before sending
+func (q Qualys) post(url string, body io.Reader) (*http.Response, error) {
 	if body == nil {
 		log.Info().Msg("adding filler body for no body post call")
 		body = bytes.NewBufferString(`<?xml version="1.0" encoding="UTF-8" ?> <ServiceRequest>
@@ -95,51 +104,79 @@ func readUnmarshal(body io.ReadCloser, s interface{}) error {
 	return nil
 }
 
-// reads a response that has a count and success. Ensures the action was successful with a count of exactly one
-func checkOne(body io.ReadCloser) error {
+func checkResponse(body io.ReadCloser) (ResponseBase, error) {
 	var r ResponseBase
 	err := readUnmarshal(body, &r)
 	if err != nil {
+		return r, err
+	}
+	if r.ResponseCode != SUCCESS {
+		return r, fmt.Errorf("non-successful response code: %s", r.ResponseCode)
+	}
+	return r, nil
+}
+
+// checkCount reads a response that has a count and success. Ensures the action was successful with a count of exactly one
+func checkCount(body io.ReadCloser, n int) error {
+	r, err := checkResponse(body)
+	if err != nil {
 		return err
 	}
-	if r.ResponseCode != "SUCCESS" {
-		return fmt.Errorf("non-successful response code: %s", r.ResponseCode)
-	}
-	if r.Count != 1 {
-		return fmt.Errorf("expected exactly 1 count, got %d", r.Count)
+	if r.Count != n {
+		return fmt.Errorf("expected exactly %d count, got %d", n, r.Count)
 	}
 	return nil
 }
 
-func (q qualys) deactivate(id string) error {
-	r, err := q.post("qps/rest/2.0/deactivate/am/asset/"+id+"?=&module=AGENT_VM%2CAGENT_PC", nil)
+func (q Qualys) deactivateByID(id string) error {
+	r, err := q.post("qps/rest/2.0/deactivateByID/am/asset/"+id+"?=&module=AGENT_VM%2CAGENT_PC", nil)
 	if err != nil {
-		return fmt.Errorf("failed to deactivate %s: %w", id, err)
+		return fmt.Errorf("failed to deactivateByID %s: %w", id, err)
 	}
-	err = checkOne(r.Body)
+	err = checkCount(r.Body, 1)
 	if err != nil {
 		return fmt.Errorf("failed to confirm deactivation %s: %w", id, err)
 	}
 	return nil
 }
 
-func (q qualys) uninstall(id string) error {
-	r, err := q.post("qps/rest/2.0/uninstall/am/asset/"+id+"?=", nil)
+func (q Qualys) uninstallByID(id string) error {
+	r, err := q.post("qps/rest/2.0/uninstallByID/am/asset/"+id+"?=", nil)
 	if err != nil {
-		return fmt.Errorf("failed to uninstall %s: %w", id, err)
+		return fmt.Errorf("failed to uninstallByID %s: %w", id, err)
 	}
-	err = checkOne(r.Body)
+	err = checkCount(r.Body, 1)
 	if err != nil {
-		return fmt.Errorf("failed to confirm uninstall %s: %w", id, err)
+		return fmt.Errorf("failed to confirm uninstallByID %s: %w", id, err)
 	}
 	return nil
 }
 
-// CleanID will deactivate the id confirm it was removed and then uninstall the id
-func (q qualys) CleanID(id string) error {
-	err := q.deactivate(id)
+// CleanID will deactivateByID the id confirm it was removed and then uninstallByID the id
+func (q Qualys) CleanID(id string) error {
+	err := q.deactivateByID(id)
 	if err != nil {
 		return err
 	}
-	return q.uninstall(id)
+	return q.uninstallByID(id)
 }
+
+// TAG BASED ACTIONS
+
+//curl --request POST \
+//--url 'https://qualysapi.qg2.apps.qualys.com/qps/rest/2.0/create/am/tag?=' \
+//--header 'authorization: Basic ZWFzdGMyYWUxOjhKZW50MzBOZC03' \
+//--header 'cache-control: no-cache' \
+//--header 'content-type: text/xml' \
+//--header 'x-requested-with: Insomnia-Testing' \
+//--cookie JSESSIONID=02871775DAB22E7D1E3B16578444A193 \
+//--data '<?xml version="1.0" encoding="UTF-8" ?>
+//<ServiceRequest>
+//<data>
+//<Tag>
+//<name>EES-smp-testtag-blah</name>
+//<color>#FFFFFF</color>
+//</Tag>
+//</data>
+//</ServiceRequest>'
+func CreateTag() {}
